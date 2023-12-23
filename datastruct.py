43c10460944481e56b6c6bf80c4d90d71b90c0bd
Ivashkka <ivashka.2.r@gmail.com>
2023-12-23 10:05:53 +0300

    socket sessions

--- datastruct.py
+++ datastruct.py
@@ -1,43 +1,86 @@
 import enum
 
-class AIMtype(enum.Enum): # ai message type
+class NAMDtype(enum.Enum): # ai data type
     request = 0
     response = 1
+    auth_data = 2
 
 #basic classes for user, response, request and session:
 
 class NAMuser:
-    def __init__(self, name, uuid):
+    __slots__ = ['type', 'name', 'uuid']
+    def __init__(self=None, name=None, uuid=None, dict=None):
+        self.type = NAMDtype.auth_data
         self.name = name
         self.uuid = uuid
+        if dict: self.from_dict(dict)
+    def to_dict(self):
+        return {"type": self.type.value, "name": self.name, "uuid": self.uuid}
+    def from_dict(self, dict):
+        self.name = dict["name"]
+        self.uuid = dict["uuid"]
+        self.type = NAMDtype(dict["type"])
 
 class AIrequest:
-    def __init__(self, message, uuid):
+    __slots__ = ['message', 'uuid', 'type']
+    def __init__(self=None, message=None, uuid=None, dict=None):
         self.message = message
         self.uuid = uuid
-        self.type = AIMtype.request
+        self.type = NAMDtype.request
+        if dict: self.from_dict(dict)
+    def to_dict(self):
+        return {"type": self.type.value, "message": self.message, "uuid": self.uuid}
+    def from_dict(self, dict):
+        self.message = dict["message"]
+        self.uuid = dict["uuid"]
+        self.type = NAMDtype(dict["type"])
 
 class AIresponse:
-    def __init__(self, message, uuid):
+    __slots__ = ['message', 'uuid', 'type']
+    def __init__(self=None, message=None, uuid=None, dict=None):
         self.message = message
         self.uuid = uuid
-        self.type = AIMtype.response
+        self.type = NAMDtype.response
+        if dict: self.from_dict(dict)
+    def to_dict(self):
+        return {"type": self.type.value, "message": self.message, "uuid": self.uuid}
+    def from_dict(self, dict):
+        self.message = dict["message"]
+        self.uuid = dict["uuid"]
+        self.type = NAMDtype(dict["type"])
 
-class NAMsession:
-    def __init__(self, uuid, user, thread):
+class NAMconnection:
+    __slots__ = ['uuid', 'user', 'client_conn', 'client_addr']
+    def __init__(self, uuid, user, client_conn, client_addr):
         self.uuid = uuid
         self.user = user
+        self.client_addr = client_addr
+        self.client_conn = client_conn
+
+class NAMsession:
+    __slots__ = ['uuid', 'client', 'thread', 'messages_history', 'text_history', '__weakref__']
+    def __init__(self, uuid, client, thread):
+        self.uuid = uuid
+        self.client = client
         self.thread = thread
         self.messages_history = [] #list of objects of type message
         self.text_history = [] #list of dictionaries for g4f requests
-        self.new_message_detected = False #serves for conditions in session thread
     
     def add_message(self, message):
         self.messages_history.append(message)
-        if (message.type == AIMtype.request):
-            self.new_message_detected = True
-            self.text_history.append({"role": "user", "content": message.message})
-        else:
-            self.text_history.append({"role": "assistant", "content": message.message})
+        match message.type:
+            case NAMDtype.request:
+                self.text_history.append({"role": "user", "content": message.message})
+            case NAMDtype.response:
+                self.text_history.append({"role": "assistant", "content": message.message})
+            case _:
+                pass
 
+    def get_username(self):
+        return self.client.user.name
     
+    def get_useruuid(self):
+        return self.client.user.uuid
+
+    def get_client_conn(self):
+        return self.client.client_conn
--- intapi/listen.py
+++ intapi/listen.py
@@ -1,32 +1,55 @@
 import socket
 import json
 
-class NAMserver(object):
+class _NAMserver(object):
     bind = False
     nam_sock = None
-    nam_conn = None
-    client_addr = None
+    encoding = 'utf-8'
+
     @staticmethod
     def bind_socket():
-        NAMserver.nam_sock = socket.socket()
-        NAMserver.nam_sock.bind(('127.0.0.1', 9090))
-        NAMserver.bind = True
+        _NAMserver.nam_sock = socket.socket()
+        _NAMserver.nam_sock.bind(('127.0.0.1', 9090))
+        _NAMserver.nam_sock.listen(3)
+        _NAMserver.bind = True
+
     @staticmethod
     def wait_for_conn():
-        NAMserver.nam_sock.listen(1)
-        NAMserver.nam_conn, NAMserver.client_addr = NAMserver.nam_sock.accept()
-        print(f"connect: {NAMserver.client_addr}")
+        client_conn, client_addr = _NAMserver.nam_sock.accept()
+        client_conn.settimeout(3)
+        data = _NAMserver.get_data(client_conn, 1024)
+        client_conn.settimeout(None)
+        return {"client_addr": client_addr, "client_conn": client_conn, "data": data}
+
+    @staticmethod
+    def get_data(client_conn, bytes):
+        return json.loads(client_conn.recv(bytes).decode())
+    
+    @staticmethod
+    def send_data(client_conn, data):
+        return client_conn.send(json.dumps(data).encode(encoding=_NAMserver.encoding))
 
 def start_server():
-    NAMserver.bind_socket()
-    NAMserver.wait_for_conn()
+    _NAMserver.bind_socket()
 
-def get_data():
-    data = NAMserver.nam_conn.recv(1024)
-    return json.loads(data)
+def wait_for_conn():
+    return _NAMserver.wait_for_conn()
 
-def send_data(message, user_name):
-    NAMserver.nam_conn.send(bytes(f"new message for user {user_name}: {message}", encoding = 'UTF-8'))
+def get_data(client_conn, bytes):
+    return _NAMserver.get_data(client_conn, bytes)
+
+def send_data(client_conn, data):
+    _NAMserver.send_data(client_conn, data)
+
+# start_server()
+# wait_for_new_conn()
+
+# def get_data():
+#     data = NAMserver.nam_conn.recv(1024)
+#     return json.loads(data)
+
+# def send_data(message, user_name):
+#     NAMserver.nam_conn.send(bytes(f"new message for user {user_name}: {message}", encoding = 'UTF-8'))
 
 
 # def send_response(message, user_name):
